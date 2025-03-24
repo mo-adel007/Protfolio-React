4194e5cfd66980ca236889586cf09b26c2586a21
mo-adel007 <67648966+mo-adel007@users.noreply.github.com>
2025-03-24 19:04:50 +0200

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,7 +7,7 @@ import { technologies } from "../constants";
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      <StarsCanvas />
+      {/* <StarsCanvas /> */}
 
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
